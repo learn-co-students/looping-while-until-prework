16b57f9eb96e79000c9a2cb3201fcb40fbfa5c47
briecodes <hello@brie.work>
2018-03-13 18:02:14 -0400

    While and Until Loops created.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,16 @@
 def using_while
 	levitation_force = 6
-	
-	#your code here
+
+	while levitation_force < 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
 end
 
-
+def using_until
+	levitation_force = 6
+	until levitation_force  == 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
+end
