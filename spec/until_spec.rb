0759616ebeb92d60c08b0153265d21b6a56e61da
github username <github email address>
2018-03-18 10:56:30 -0400

    Done.

--- spec/until_spec.rb
+++ spec/until_spec.rb
@@ -2,8 +2,8 @@ require_relative "spec_helper"
 require_relative "../lib/until.rb"
 
 describe "#using_until" do
-	it "puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
-	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	it "puts out the phrase 'I will just loop up to 10 times.' until the levitation force is incremented to 10" do
+	  looping_string = "I will just loop up to 10 times.\nI will just loop up to 10 times.\nI will just loop up to 10 times.\nI will just loop up to 10 times.\n"
 	  expect{ using_until }.to output(looping_string).to_stdout
   end
 end
