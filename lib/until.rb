b57af9a16f4bcd82ac89666f885153d1b5d71404
Shawn Koski <shawnnkoski@gmail.com>
2018-03-29 03:05:14 +0000

    While and until lopps

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
         levitation_force = 6
         #your code here
+        until levitation_force == 
+          puts "Wingardium Leviosa"
+          levitation_force += 1
+        end
     
 end
 
