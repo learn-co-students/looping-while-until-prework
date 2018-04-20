8e21f34d3ef6cc2c107025e394e9d52511302285
github username <github email address>
2018-04-19 23:52:06 -0700

    re: until.rb // #using_until added until loop

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
-def using_until
-        levitation_force = 6
-        #your code here
-    
-end
+levitation_force = 6
 
+def using_until
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    using_until += 1
+  end
+end
