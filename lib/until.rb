c127315d3617c0d14ce2de4062111d2d0ce44049
Bryan Teng <bteng1226@gmail.com>
2018-06-23 22:35:21 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
         levitation_force = 6
-        #your code here
+        until levitation_force == 10
+        puts "Plus Ultra"
+        levitation_force+=1
+      end
     
 end
 
