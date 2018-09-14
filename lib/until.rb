3c2f4e2da8a667a31188c5209b0424e6ee4b251d
Scott M <gmat191@gmail.com>
2018-09-14 21:55:16 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
 def using_until
         levitation_force = 6
-        #your code here
-    
+        until levitation_force == 10 do
+        puts "Wingardium Leviosa"
+        levitation_force += 1
 end
-
+end
+#Be careful of those type errors, I failed the test multiple times because I failed to catch it.
