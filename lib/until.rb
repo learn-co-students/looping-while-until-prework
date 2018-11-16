15ef9296b5a6b826dbdf87391fcf9b048f27844e
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-16 04:55:55 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
         levitation_force = 6
         #your code here
-    
+        until levitation_force == 10
+          looping_string = "Wingardium Leviosa"
+          puts looping_string
+          levitation_force += 1
+        end
 end
-
