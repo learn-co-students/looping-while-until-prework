bb33ec563536ae0b49f62083a9cdab275ee731ca
cyn7117 <cyn7117@gmail.com>
2018-11-17 21:20:33 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,10 @@
 def using_until
         levitation_force = 6
+        until levitation_force == 10
+        levitation_force ++
+        puts 'Wingardium Leviosa'
+      end
+        
         #your code here
     
 end
