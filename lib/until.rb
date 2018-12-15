1c44069c722bc3934a085c48ea0fbd74c0db5ac7
abstract-animation-9914 <vm930@hotmail.com>
2018-12-15 06:44:08 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
         levitation_force = 6
-        #your code here
-    
+        until levitation_force == 0
+        do levitation_force = levitation_force -1
+        break
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
+
 def using_while
 	levitation_force = 6
-	
+	while levitation_force > 0 do 
+	puts "Wingardium Leviosa"
+	levitation_force = levitation_force-1
 	#your code here
+	break
 end
 
 
